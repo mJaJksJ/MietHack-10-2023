ffc0160157387c8f958871092a22ca47b1307797
mJaJksJ <maksim.m00@mail.ru>
2023-10-22 10:37:04 +0300

    chore: миграция

--- MietHack-10-2023/Database/Migrations/20231022073628_FixUser.Designer.cs
+++ MietHack-10-2023/Database/Migrations/20231022073628_FixUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MietHack_10_2023.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MietHack_10_2023.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20231022073628_FixUser")]
+    partial class FixUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MietHack-10-2023/Database/Migrations/20231022073628_FixUser.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MietHack_10_2023.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Students_student_id",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "linq_to_gruop",
+                table: "Events",
+                newName: "linq_to_group");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "student_id",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "logo",
+                table: "StudentUnions",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Students_student_id",
+                table: "Users",
+                column: "student_id",
+                principalTable: "Students",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Students_student_id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "logo",
+                table: "StudentUnions");
+
+            migrationBuilder.RenameColumn(
+                name: "linq_to_group",
+                table: "Events",
+                newName: "linq_to_gruop");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "student_id",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Students_student_id",
+                table: "Users",
+                column: "student_id",
+                principalTable: "Students",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
